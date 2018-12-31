8e36e9b4888dcda972d83328ed365e0f1ed2e563
Joonhee <pakjoonhee90@gmail.com>
2018-12-30 20:53:10 -0800

    async works

--- src/components/Movies.vue
+++ src/components/Movies.vue
@@ -1,45 +1,32 @@
 <template>
-  <div class="hello">
-    <div v-if="results">
-        <div class="row" v-for="(result,index) in results" @click="getMovieDetails(index)">
-          <img class="poster" v-bind:src="'http://image.tmdb.org/t/p/w185/' + result.poster_path"/>
-          <p class="title">{{result.title}}</p>
-        </div>
-    </div>
-  
-
+  <div id="app">
+    <p>hello</p>
+  <div>
+    <ul>
+      <li v-for="post in posts">
+        <h1>
+          {{post.title}}
+        </h1>
+        <p>
+          {{post.body}}
+        </p>
+      </li>
+    </ul>
   </div>
+</div>
 </template>
 <script>
 
 import axios from 'axios';
+import {mapState} from 'vuex'
 export default {
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App',
-      code: '',
-      results: '',
-    }
-  },
+  
+  computed: mapState(['posts', 'loading']),
   created() {
-    this.movieCall()
+    console.log(this.$store)
+  	this.$store.dispatch('loadData') // dispatch loading
   },
-  methods: {
-    movieCall() {
-      axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=69ddc9ad6327590c413357deb829550e&language=en-US&page=1`)
-      .then(response => {
-        this.results = response.data.results;
-      })
-      .catch(e => {
-        console.log(e)
-      })
-    },
-    getMovieDetails(index) {
-      console.log(index);
-      var myProp = this.results[index];
-      this.$router.push({name:'MovieDetails', params:{Pid:index, Data:myProp}});
-    }
-  },
+  
   
 }
 </script>
@@ -50,18 +37,23 @@ export default {
   display: inline-block;
   padding: 10px;
 }
-
 .poster{
   width: 185px;
   height: 274px;
 }
-
 .title{
   overflow: hidden;
   width: 185px;
   height: 40px;
   font-size: 15px;
 }
-
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
 
 </style>
--- src/store/store.js
+++ src/store/store.js
@@ -2,10 +2,30 @@ import Vue from 'vue';
 import Vuex from 'vuex';
 
 Vue.use(Vuex);
+Vue.config.devtools = true
+import axios from 'axios'
+const URL = 'https://jsonplaceholder.typicode.com/posts';
 
 export const store = new Vuex.Store({
-  state: {
-    message: "hello from vuex",
-    counter: 0
+	state: {
+  	posts: [],
+    loading: true
+  },
+  actions: {
+  	loadData({commit}) {
+    	axios.get(URL).then((response) => {
+    	// console.log(response.data, this)
+        commit('updatePosts', response.data)
+        commit('changeLoadingState', false)
+    	})
+    }
+  },
+  mutations: {
+  	updatePosts(state, posts) {
+    	state.posts = posts
+    },
+    changeLoadingState(state, loading) {
+    	state.loading = loading
+    }
   }
 })
